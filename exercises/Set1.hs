0f51903be42f00d2d6f8d94f84a3bd85af905f26
Alaville <72505209+Alaville@users.noreply.github.com>
2020-10-10 12:53:02 +0300

    Update Set1.hs

--- exercises/Set1.hs
+++ exercises/Set1.hs
@@ -56,7 +56,7 @@ quadruple x = double x * 2
 --   distance 0 0 1 1  ==>  1.4142135...
 --   distance 1 1 4 5  ==>  5.0
 
-distance :: Int -> Int -> Int -> Int -> Double
+distance :: Double -> Double -> Double -> Double -> Double
 distance x1 y1 x2 y2 = sqrt((x2 - x1)^2 + (y2 - y1)^2)
 
 ------------------------------------------------------------------------------
