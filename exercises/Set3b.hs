5ae8c514600d1e0cef99e1818b9be5d6cf8f3af8
Alaville <72505209+Alaville@users.noreply.github.com>
2020-10-23 14:34:41 +0300

    Update Set3b.hs

--- exercises/Set3b.hs
+++ exercises/Set3b.hs
@@ -64,7 +64,8 @@ sums i = todo
 --   mylast 0 [1,2,3] ==> 3
 
 mylast :: a -> [a] -> a
-mylast def xs = todo
+mylast def [] = def
+mylast def xs = xs !! (length xs - 1)
 
 ------------------------------------------------------------------------------
 -- Ex 4: safe list indexing. Define a function indexDefault so that
