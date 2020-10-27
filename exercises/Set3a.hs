16acb3ca1595a47071f466616e24b8b658e14af1
Alaville <72505209+Alaville@users.noreply.github.com>
2020-10-27 11:16:09 +0200

    Update Set3a.hs

--- exercises/Set3a.hs
+++ exercises/Set3a.hs
@@ -149,7 +149,9 @@ powers k max = takeWhile (<= max) (myPowers k)
 --     ==> Avvt
 
 while :: (a->Bool) -> (a->a) -> a -> a
-while check update value = todo
+while check update value
+  | check value == True = while check update (update value)
+  | otherwise = value
 
 ------------------------------------------------------------------------------
 -- Ex 8: another version of a while loop. This time, the check
