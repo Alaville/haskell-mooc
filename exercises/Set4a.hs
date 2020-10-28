f74b6a3fe9edf19633554f6dd445685cc9f1c469
Alaville <72505209+Alaville@users.noreply.github.com>
2020-10-28 14:48:36 +0200

    Update Set4a.hs

--- exercises/Set4a.hs
+++ exercises/Set4a.hs
@@ -152,7 +152,9 @@ average xs = realToFrac (sum xs) / genericLength xs
 --     ==> "Bob"
 
 winner :: Map.Map String Int -> String -> String -> String
-winner scores player1 player2 = todo
+winner scores player1 player2
+  | Map.findWithDefault 0 player1 scores >= Map.findWithDefault 0 player2 scores = player1
+  | otherwise = player2
 
 ------------------------------------------------------------------------------
 -- Ex 9: compute how many times each value in the list occurs. Return
