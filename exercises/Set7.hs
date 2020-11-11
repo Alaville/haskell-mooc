dc01e9ed84390547a892106806fa860f5abb6a93
Alaville <72505209+Alaville@users.noreply.github.com>
2020-11-11 14:27:01 +0200

    Update Set7.hs

--- exercises/Set7.hs
+++ exercises/Set7.hs
@@ -56,7 +56,7 @@ member :: Eq a => a -> Set a -> Bool
 member x (Set xs) = elem x xs 
 
 -- add a member to a set
-add :: a -> Set a -> Set a
+add :: Eq a => a -> Set a -> Set a
 add x (Set xs)
   | elem x xs = Set xs
   | otherwise = Set (x:xs)
