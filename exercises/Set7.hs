a75144a549af271e6cfa1a81dd6be5493f6f7a32
Alaville <72505209+Alaville@users.noreply.github.com>
2020-11-17 13:57:10 +0200

    Update Set7.hs

--- exercises/Set7.hs
+++ exercises/Set7.hs
@@ -121,7 +121,7 @@ average xs = (sum xs / fromIntegral (length xs))
 -- Ex 5: reverse a NonEmpty list.
 
 reverseNonEmpty :: NonEmpty a -> NonEmpty a
-reverseNonEmpty = todo
+reverseNonEmpty xs = Data.List.NonEmpty.reverse xs
 
 ------------------------------------------------------------------------------
 -- Ex 6: implement Semigroup instances for the Distance, Time and
