95d96b30055b986c87e25633b1381c07260a9b1a
Alaville <72505209+Alaville@users.noreply.github.com>
2020-11-19 17:07:00 +0200

    Update Set3b.hs

--- exercises/Set3b.hs
+++ exercises/Set3b.hs
@@ -110,7 +110,7 @@ sorted (x:y:xs) = if x <= y then sorted (y:xs) else False
 
 sumsOf :: [Int] -> [Int]
 sumsOf [] = []
-sumsOf (x:b) = x : (x+b) 
+sumsOf (x:y) = x : (x+y) : []
 sumsOf (a:b:xs) = a : (a+b) : sumsOf (b:xs)
 
 ------------------------------------------------------------------------------
